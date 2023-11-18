f5e75ddc7ec477d0716e2c3bf72b91e3ef8a9643
hoarfroster <hoarfroster@outlook.com>
2023-11-18 20:35:12 +0800

    refactor project, remove redundant files

--- lib/models/bundleable.ts
+++
@@ -1,9 +0,0 @@
-class Bundleable {
-  publicId: string;
-  title: string;
-
-  constructor(publicId: string, title: string) {
-    this.publicId = publicId;
-    this.title = title;
-  }
-}
--- lib/ms-graph/secret.ts
+++
@@ -1,6 +0,0 @@
-export interface MSGraphSecret {
-  dskUserID: string;
-  clientID: string;
-  tenantID: string;
-  clientSecret: string;
-}
