243afb5c8f2cc2a494b1133288c3c3cd0ce6ea65
hoarfroster <hoarfroster@outlook.com>
2023-12-01 21:26:28 +0800

    feat: new card style

--- src/app/recipe/recipe-item.tsx
+++ src/app/recipe/recipe-item.tsx
@@ -1,7 +1,8 @@
 import Recipe from "@/lib/models/recipe";
 import Link from "next/link";
+import LazyExtendedImage from "@/components/ui/extended-image";
 
-export default function RecipeItem({ recipe }: Readonly<{ recipe: Recipe }>) {
+export function RecipeItem({recipe}: Readonly<{ recipe: Recipe }>) {
     return (
         <div className="py-4">
             <Link href={`/recipe/${recipe.publicId}`}>
@@ -13,4 +14,33 @@ export default function RecipeItem({ recipe }: Readonly<{ recipe: Recipe }>) {
             </p>
         </div>
     );
+}
+
+export function RecipeCard({recipe}: Readonly<{ recipe: Recipe }>) {
+    let img = <></>;
+    if (recipe.odCover !== null) {
+        img =
+            <LazyExtendedImage src={recipe.odCover} alt="Cover" className="rounded-t-lg"/>;
+    }
+    return (<div
+        className="w-[300px] shrink-0 mx-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 inline-block">
+        <Link href={`/recipe/${recipe.publicId}`}>
+            {img}
+        </Link>
+        <div className="p-5">
+            <Link href={`/recipe/${recipe.publicId}`}>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{recipe.title}</h5>
+            </Link>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{recipe.description}</p>
+            <Link href={`/recipe/${recipe.publicId}`}
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Read more
+                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                     fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                          d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </Link>
+        </div>
+    </div>);
 }
