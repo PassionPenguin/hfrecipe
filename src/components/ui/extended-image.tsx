4d96c16179bdfeaf74a8e2b0092309d63272b74b
hoarfroster <hoarfroster@outlook.com>
2023-12-10 15:02:42 +0800

    feat: support graph images on client side components

--- src/components/ui/extended-image.tsx
+++ src/components/ui/extended-image.tsx
@@ -1,27 +1,44 @@
-import Loading, {LoadingSkeletonType} from "@/components/loading";
-import {MSGraphClient} from "@/lib/ms-graph/client";
-import {Suspense} from "react";
+import Loading, { LoadingSkeletonType } from "@/components/loading";
+import { MSGraphClient } from "@/lib/ms-graph/client";
+import { Suspense } from "react";
 
 async function ExtendedImage({
-                                 src,
-                                 alt,
-                                 className
-                             }: {
+    src,
+    alt,
+    className
+}: {
     src: string;
     alt: string;
     className: string;
 }) {
     if (src.startsWith("https://msgraph.hoarfroster.space/")) {
+        if(typeof window !== 'undefined') {
+            let headers = new Headers();
+            headers.append("GraphPath",
+                src.replace("https://msgraph.hoarfroster.space/", "/"));
+            return fetch("/api/graph/download", {headers: headers}).then(async (res) => {
+                if (res.ok) {
+                    const result_1 = await res.json();
+                    if (result_1['@odata.context'] !== undefined) {
+                        return <img src={result_1['@microsoft.graph.downloadUrl']} alt={alt} className={className}/>;
+                    } else {
+                        return <></>;
+                    }
+               }
+            });
+        }
         return MSGraphClient.driveProvider
-            .getDriveItemURL(src.replace("https://msgraph.hoarfroster.space/", "/"))
+            .getDriveItemURL(
+                src.replace("https://msgraph.hoarfroster.space/", "/")
+            )
             .then((result) => {
                 if (result !== null) {
-                    return <img src={result} alt={alt} className={className}/>;
+                    return <img src={result} alt={alt} className={className} />;
                 } else {
                     return <></>;
                 }
             });
-    } else return <>Incorrect path.</>;
+    } else return <img src={src} alt={alt} />;
 }
 
 export default function LazyExtendedImage(props: {
@@ -30,7 +47,7 @@ export default function LazyExtendedImage(props: {
     className: string;
 }) {
     return (
-        <Suspense fallback={<Loading type={LoadingSkeletonType.image}/>}>
+        <Suspense fallback={<Loading type={LoadingSkeletonType.image} />}>
             <ExtendedImage
                 src={props.src}
                 alt={props.alt}
