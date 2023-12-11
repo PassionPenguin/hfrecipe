fd24c6ae3e4b9afb7adf267673e9078702eed6f4
hoarfroster <hoarfroster@outlook.com>
2023-12-11 21:42:03 +0800

    update: auth

---
+++ src/lib/auth/protectRequestRoutes.ts
@@ -0,0 +1,27 @@
+import {UserRole} from "@/app/user/usermodel";
+import {protectRoutes} from "@/lib/auth/protectRoutes";
+import {NextRequest} from "next/server";
+import {NextApiRequest} from "next";
+
+export function protectRequestRoutes(request: NextRequest): {
+    status: boolean;
+    userType: UserRole;
+    userName: string | null;
+} {
+    return protectRoutes({
+        pathname: request.nextUrl.pathname,
+        token: request.cookies.get("TOKEN")?.value ?? ""
+    });
+}
+
+export function protectAPIRequestRoutes(request: NextApiRequest): {
+    status: boolean;
+    userType: UserRole;
+    userName: string | null;
+} {
+    return protectRoutes({
+            pathname: (new URL(request.url).pathname),
+            token: (request as unknown as NextRequest).cookies.get("TOKEN").value ?? ""
+        }
+    )
+}
